0845cc1a0d9e243037cf4eabd770e9cf42d0068b
jkazuk <jeffreykazuk@gmail.com>
2021-04-24 15:33:54 -0500

    one punch girl
    three actually

--- Mina/Mina.gd
+++ Mina/Mina.gd
@@ -21,8 +21,8 @@ var on_ladder = false
 var dir = 1
 var on_wall = false
 var which_wall = 0
+var current_attack = "none"
 #var jump_particle = load("res://land.tscn")
-var current_zone = 'village'
 var state_machine :AnimationNodeStateMachinePlayback
 onready var jt = $'jump_timer'
 #onready var camerapos = $camerapos
@@ -45,21 +45,22 @@ func _physics_process(delta):
 	slide(delta)
 	motion = move_and_slide(motion, UP)
 func slide(delta):
-	if on_ladder and Input.is_action_pressed("grab"):
-		climbing = true
-		on_wall = true
-		if motion.y >0 and climbing:
-			print("grabbed")
-			motion.y = 0 + Input.get_action_strength("down")*500 - Input.get_action_strength("up")*500
-			motion.x = 0 + Input.get_action_strength("right")*500 - Input.get_action_strength("left")*500
-	else:
-		climbing = false
-	if on_wall and Input.is_action_pressed("grab"):
-		sliding = true
-		if motion.y >0:
-			motion.y += (-GRAVITY+200)*delta
-	else:
-		sliding = false
+	# if on_ladder and Input.is_action_pressed("grab"):
+	# 	climbing = true
+	# 	on_wall = true
+	# 	if motion.y >0 and climbing:
+	# 		print("grabbed")
+	# 		motion.y = 0 + Input.get_action_strength("down")*500 - Input.get_action_strength("up")*500
+	# 		motion.x = 0 + Input.get_action_strength("right")*500 - Input.get_action_strength("left")*500
+	# else:
+	# 	climbing = false
+	# if on_wall and Input.is_action_pressed("grab"):
+	# 	sliding = true
+	# 	if motion.y >0:
+	# 		motion.y += (-GRAVITY+200)*delta
+	# else:
+	# 	sliding = false
+	sliding = false
 	
 func direction():
 	match dir:
@@ -72,6 +73,21 @@ func direction():
 func movement(friction):
 	var current = state_machine.get_current_node()
 
+	if Input.is_action_just_pressed("attack_1"):
+
+		if current != "punch1" and current != "punch2" and current != "kick":
+			state_machine.travel("punch1")
+
+		elif current == "punch1":
+			state_machine.travel("punch2")
+
+		elif current == "punch2":
+			state_machine.travel("kick")
+
+
+
+	
+
 	if Input.is_action_just_pressed("down"):
 		set_collision_mask_bit(1, false)
 	if Input.is_action_just_released("down"):
@@ -79,25 +95,28 @@ func movement(friction):
 	
 	if Input.is_action_just_released("jump") and motion.y < 0:
 		motion.y = lerp(motion.y, 0, 0.5)	
-
-	if Input.is_action_pressed("right"):
-		if not on_ice and motion.x < 0 and is_on_floor():
-				
-			motion.x = lerp(motion.x, 0, 0.5)
-			
-		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
-		
-		dir = -1
-	elif Input.is_action_pressed("left"):
-			if not on_ice and motion.x > 0 and is_on_floor():
-				
+	if current != "punch1" and current != "punch2" and current != "kick":
+		if Input.is_action_pressed("right"):
+			if not on_ice and motion.x < 0 and is_on_floor():
+					
 				motion.x = lerp(motion.x, 0, 0.5)
 				
-			motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+			motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 			
-			dir = 1
+			dir = -1
+		elif Input.is_action_pressed("left"):
+				if not on_ice and motion.x > 0 and is_on_floor():
+					
+					motion.x = lerp(motion.x, 0, 0.5)
+					
+				motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+				
+				dir = 1
+		else:
+			friction = true
 	else:
-		friction = true
+		motion.x = lerp(motion.x, 0, FRICTION)
+
 	if is_on_floor() and canmove or climbing: 
 
 		if Input.is_action_just_pressed("jump"):
@@ -107,14 +126,15 @@ func movement(friction):
 		if friction == true and not on_ice:
 			motion.x = lerp(motion.x, 0, FRICTION)
 
-	if motion.y > 100 and not ray_on_floor():
-		state_machine.travel("falling")
-	elif motion.y < 48:
-		state_machine.travel("jump")
-	elif motion.x != 0:
-		state_machine.travel("run")
-	elif ray_on_floor() and motion.x == 0:
-		state_machine.travel("idle")
+	if current != "punch1" and current != "punch2" and current != "kick":
+		if motion.y > 100 and not ray_on_floor():
+			state_machine.travel("falling")
+		elif motion.y < 48:
+			state_machine.travel("jump")
+		elif motion.x != 0 and current != "run":
+			state_machine.travel("run")
+		elif ray_on_floor() and motion.x == 0 and current != "idle":
+			state_machine.travel("idle")
 
 
 
@@ -131,6 +151,9 @@ func _on_jump_timer_timeout():
 		GRAVITY = 9.8 * 250
 	else:
 		GRAVITY = 9.8 * 1000
+
+func set_attack(attack):
+	current_attack = attack
 
 #func pass_camera_shake(amount):
 #	camera.add_trauma(amount)
