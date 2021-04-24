b201f58cea3628d48073b2a624dcad1b3a36067e
Shane <shanescott86@gmail.com>
2021-04-24 09:30:43 -0500

    ghost enemy exists

--- enemies/library/ghost/ghost.gd
+++ enemies/library/ghost/ghost.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var run_speed = 15
+var run_speed = 65
 var velocity = Vector2.ZERO
 var health = 45
 var player = null
@@ -10,9 +10,15 @@ func _physics_process(delta):
 	if player:
 		velocity = position.direction_to(player.position) * run_speed
 	velocity = move_and_slide(velocity)
+	
+	if velocity.x >= 0:
+		$AnimatedSprite.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite.flip_h = true
+		
 
 func _on_line_of_sight_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Mina":
 		player = body
 
 func _on_line_of_sight_body_exited(body):
@@ -25,3 +31,9 @@ func take_damage(dmg):
 
 func die():
 	self.queue_free()
+
+
+func _on_damage_radius_body_entered(body):
+	if body == player:
+		pass
+		#player takes damage based on time spent in zone
