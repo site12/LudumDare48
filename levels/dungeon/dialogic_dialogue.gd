68f6e565e575a3d34220b56c7c1949ec3aefee4d
Shane <shanescott86@gmail.com>
2021-04-26 02:11:31 -0500

    bruh

---
+++ levels/dungeon/dialogic_dialogue.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+var begun = false
+onready var mina = get_parent().get_node("Mina")
+onready var audio = get_tree().get_root().get_node("root/audio")
+onready var parentnode = get_tree().get_root().get_node("root/speaker_pos/Control")
+func _on_dialogic_dialogue_body_entered(body):
+	if not begun and body.name == "Mina":
+		
+		var gd = Dialogic.start("dungeon")
+		parentnode.add_child(gd)
+		parentnode.get_parent().get_node("AnimatedSprite").visible = true
+		begun = true
+		
+		gd.connect('timeline_end', self, 'after_dialog')
+		yield(get_tree().create_timer(.2), "timeout")
+		mina.canmove = false
+
+func after_dialog(timeline_name):
+	mina.canmove = true
+	mina.toggle_hud()
+	audio.get_node("AnimationPlayer").play("fade_in")
+	parentnode.get_parent().get_node("AnimatedSprite").queue_free()
+	parentnode.queue_free()
--- levels/library/bosshealth.gd
+++ levels/library/bosshealth.gd
@@ -1,9 +1,10 @@
 extends Control
 
 
-onready var boss = get_tree().get_root().get_node("root/bigknight")
+#onready var boss = get_tree().get_root().get_node("root/bigknight")
 
 func _physics_process(delta):
-	$ProgressBar.value = boss.health
-	if boss.health <=0:
-		self.queue_free()
+	#$ProgressBar.value = boss.health
+	#if boss.health <=0:
+	#	self.queue_free()
+	pass
