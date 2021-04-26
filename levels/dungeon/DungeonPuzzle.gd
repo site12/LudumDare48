2ada85112567f6fb84ad3a2492f59a830ec3763b
megankozun <megan.kozun@gmail.com>
2021-04-25 22:09:15 -0500

    door in library

--- levels/dungeon/DungeonPuzzle.gd
+++ levels/dungeon/DungeonPuzzle.gd
@@ -13,6 +13,16 @@ func _on_Button_pressed():
 	if order.size() == 3:
 		check_order()
 
+func _on_Button2_pressed():
+	order.append("snake")
+	if order.size() == 3:
+		check_order()
+
+func _on_Button3_pressed():
+	order.append("bird")
+	if order.size() == 3:
+		check_order()
+
 func check_order():
 	if order == correct:
 		$Control/Light_Green.visible = true
@@ -24,3 +34,11 @@ func check_order():
 		$Control/Light_Red.visible = true
 		yield(get_tree().create_timer(2), "timeout")
 		$Control/Light_Red.visible = false
+		$Control/Button.pressed = false
+		$Control/Button2.pressed = false
+		$Control/Button3.pressed = false
+
+
+func _on_Button4_pressed():
+	self.visible = false
+	order.clear()
