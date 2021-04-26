7d61121290285d1614a868f5254184ff29a12e44
jkazuk <jeffreykazuk@gmail.com>
2021-04-25 21:59:18 -0500

    lights n lightning

--- menus/main_menu/menu.gd
+++ menus/main_menu/menu.gd
@@ -6,13 +6,16 @@ func _ready():
 func _physics_process(delta):
 	$Camera2D.position.x +=1
 	$Node2D/Sprite.position.x +=1
-	$ParallaxBackground/ParallaxLayer2/AnimatedSprite.position.x+=0.3
+	# $ParallaxBackground/ParallaxLayer2/AnimatedSprite.position.x+=0.3
 	
 
 
 func _on_Timer_timeout():
 	randomize()
-	$ParallaxBackground/ParallaxLayer2/AnimatedSprite.visible = true
+	# $ParallaxBackground/ParallaxLayer2/AnimatedSprite.visible = true
+	$Camera2D/lightning/AnimationPlayer.play("lightning" + str(randi()%5))
+	# $ParallaxBackground/ParallaxLayer3.visible = true
 	yield(get_tree().create_timer(.1), "timeout")
-	$ParallaxBackground/ParallaxLayer2/AnimatedSprite.visible = false
+	# $ParallaxBackground/ParallaxLayer3.visible = false
+	# $ParallaxBackground/ParallaxLayer2/AnimatedSprite.visible = false
 	$Timer.wait_time = rand_range(3,5)
